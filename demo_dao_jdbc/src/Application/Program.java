85c49a25044c88dea4308e4aa2b8179d72b7851a
Giovani-Gomes <giovanigomes2002@gmail.com>
2025-02-08 20:36:13 -0300

    Seller entity class

--- demo_dao_jdbc/src/Application/Program.java
+++ demo_dao_jdbc/src/Application/Program.java
@@ -1,11 +1,17 @@
 package Application;
 
 import model.entites.Departament;
+import model.entites.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Departament departament = new Departament(1,"Livros");
         System.out.println(departament);
+
+        Seller seller = new Seller(1,"Pedro","Pedro@gmail.com", new Date(),3000.0,departament);
+        System.out.println(seller);
     }
 
 }
