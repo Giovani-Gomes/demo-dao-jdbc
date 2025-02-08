e91a95465ae4d7b32fe16427eed85f3c00dfaf1c
Giovani-Gomes <giovanigomes2002@gmail.com>
2025-02-08 20:24:40 -0300

    Department entity class

---
+++ demo_dao_jdbc/src/Application/Program.java
@@ -0,0 +1,11 @@
+package Application;
+
+import model.entites.Departament;
+
+public class Program {
+    public static void main(String[] args) {
+        Departament departament = new Departament(1,"Livros");
+        System.out.println(departament);
+    }
+
+}
